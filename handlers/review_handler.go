7f1b663fd4c66e4b2d37af37765751bca82895df
truongtamthanh2004 <truongtamthanh2004@gmail.com>
2025-07-17 10:33:33 +0700

    review like comment

--- handlers/review_handler.go
+++ handlers/review_handler.go
@@ -8,6 +8,7 @@ import (
 	"oe02_go_tam/services"
 	"oe02_go_tam/utils"
 	"strconv"
+	"strings"
 )
 
 type ReviewHandler struct {
@@ -38,4 +39,90 @@ func (h *ReviewHandler) GetReviews(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, response)
+}
+
+type ReviewRequest struct {
+	TourID  uint   `json:"tour_id" binding:"required"`
+	Rating  int    `json:"rating" binding:"required"`
+	Content string `json:"content" binding:"required"`
+}
+
+func (h *ReviewHandler) CreateReview(c *gin.Context) {
+	var req ReviewRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	userID := c.GetUint("user_id")
+
+	review, err := h.service.CreateReview(userID, req.TourID, req.Rating, req.Content)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Create review failed"})
+		return
+	}
+
+	reviewResponse := utils.MapReviewToResponse(*review)
+
+	c.JSON(http.StatusCreated, reviewResponse)
+}
+
+func (h *ReviewHandler) GetOwnReview(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": constant.T("review.id_invalid")})
+		return
+	}
+	userID := c.GetUint("user_id")
+
+	review, err := h.service.GetOwnReview(uint(id), userID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": constant.T("review.not_found")})
+		return
+	}
+
+	reviewResponse := utils.MapReviewToResponse(*review)
+
+	c.JSON(http.StatusOK, reviewResponse)
+}
+
+func (h *ReviewHandler) UpdateReview(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": constant.T("review.id_invalid")})
+		return
+	}
+
+	var req ReviewRequest
+	if err := c.ShouldBindJSON(&req); err != nil || strings.TrimSpace(req.Content) == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": constant.T("review.invalid_input")})
+		return
+	}
+
+	userID := c.GetUint("user_id")
+
+	review, err := h.service.UpdateReview(uint(id), userID, req.Rating, req.Content)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": constant.T("review.update_failed")})
+		return
+	}
+
+	reviewResponse := utils.MapReviewToResponse(*review)
+	c.JSON(http.StatusOK, reviewResponse)
+}
+
+func (h *ReviewHandler) DeleteReview(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": constant.T("review.id_invalid")})
+		return
+	}
+
+	userID := c.GetUint("user_id")
+
+	if err := h.service.DeleteReview(uint(id), userID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": constant.T("review.delete_failed")})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": constant.T("review.delete_success")})
 }
